83ed3ad9d4889952894fc9cd8572c4f72beb30e6
amogh bihani <amogh1001@gmail.com>
2016-03-14 22:31:25 +0530

    Creating webapp for siso bus tracker

---
+++ webapp/scripts/main.js
@@ -0,0 +1,111 @@
+var mFirebaseRef = new Firebase("https://sisobustracker.firebaseio.com/RouteNumber/");
+
+var mMap;
+var mMapBounds;
+var mRouteNumber = "9";
+var mIsWaiting = true;
+var mOfficeLocation;
+
+var mOfficeMarker;
+var mUserMarker;
+var mBusMarker;
+
+var mUserLocationListener;
+
+function initMap() {
+  loadUi();
+
+  mOfficeLocation = new google.maps.LatLng(12.980253, 77.697375);
+  mMapBounds = new google.maps.LatLngBounds();
+  mMapBounds.extend(mOfficeLocation);
+
+  var mapOptions = {
+    center: mOfficeLocation,
+    zoom: 15
+  };
+  mMap = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+  mOfficeMarker = new google.maps.Marker({
+    position: mOfficeLocation,
+    title: "Office",
+    icon: "res/office_building.png",
+  });
+  mOfficeMarker.setMap(mMap);
+
+  mUserMarker = new google.maps.Marker({
+    title: "User",
+    icon: "res/user.png",
+  });
+  mUserMarker.setMap(mMap);
+  getUserLocation();
+
+  mBusMarker = new google.maps.Marker({
+    title: "Bus",
+    icon: "res/bus.png",
+  });
+  mBusMarker.setMap(mMap);
+  getBusLocation();
+}
+
+function getUserLocation() {
+  if (navigator.geolocation == null) {
+    return;
+  }
+
+  mUserLocationListener = navigator.geolocation.watchPosition(setUserLocation);
+}
+
+function setUserLocation(position) {
+  var latitude = position.coords.latitude;
+  var longitude = position.coords.longitude;
+  var shouldResizeBounds = false;
+  if (mUserMarker.getPosition() == null) {
+    shouldResizeBounds = true;
+  }
+  mUserMarker.setPosition(new google.maps.LatLng(latitude, longitude));
+  if (shouldResizeBounds) {
+    mMapBounds.extend(mUserMarker.getPosition());
+    updateMapBounds();
+  }
+
+  if (!mIsWaiting) {
+    updateBusLocation(latitude, longitude);
+  }
+}
+
+function getBusLocation() {
+  mFirebaseRef.child(mRouteNumber).on("value", function(snapshot) {
+    setBusLocation(snapshot.val().latitude, snapshot.val().longitude);
+  }, function(error) {
+    console.log(error.code);
+  });
+}
+
+function setBusLocation(latitude, longitude) {
+  var shouldResizeBounds = false;
+  if (mBusMarker.getPosition() == null) {
+    shouldResizeBounds = true;
+  }
+  mBusMarker.setPosition(new google.maps.LatLng(latitude, longitude));
+  if (shouldResizeBounds) {
+    mMapBounds.extend(mBusMarker.getPosition());
+    updateMapBounds();
+  }
+}
+
+function updateBusLocation(lat, long) {
+  mFirebaseRef.child(mRouteNumber).set({latitude: lat, longitude: long});
+}
+
+function updateMapBounds() {
+  mMap.fitBounds(mMapBounds);
+}
+
+function loadUi() {
+  document.getElementById("routeNumber").innerHTML = "R:" + mRouteNumber;
+  document.getElementById("waitingButton").checked = "checked";
+}
+
+function onStateChange(isWaiting) {
+  mIsWaiting = isWaiting;
+}
